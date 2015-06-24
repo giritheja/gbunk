f5c3795962cd00be67a971c9268e5d7464820b67
S Giritheja <s.giritheja@gmail.com>
2015-06-24 15:54:12 +0530

    Added geolocation functionality in ionic

---
+++ gbunk/www/js/app.js
@@ -0,0 +1,32 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.services'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
+.controller('locator',function($scope,geolocation){
+  $scope.location=function(){
+    geolocation.getCurrentPosition().then(function(data){
+      console.log(data.coords.accuracy);
+      $scope.latitude=data.coords.latitude;
+      $scope.longitude=data.coords.longitude;
+      $scope.altitude=data.coords.altitude;
+      $scope.accuracy=data.coords.accuracy;
+    }) 
+  }
+});
---
+++ gbunk/www/js/services.js
@@ -0,0 +1,43 @@
+angular.module('starter.services', [])
+  .factory('geolocation', ['$q', function ($q) {
+
+    return {
+      getCurrentPosition: function (options) {
+        var q = $q.defer();
+
+        navigator.geolocation.getCurrentPosition(function (result) {
+          q.resolve(result);
+        }, function (err) {
+          q.reject(err);
+        }, options);
+
+        return q.promise;
+      },
+
+      watchPosition: function (options) {
+        var q = $q.defer();
+
+        var watchID = navigator.geolocation.watchPosition(function (result) {
+          q.notify(result);
+        }, function (err) {
+          q.reject(err);
+        }, options);
+
+        q.promise.cancel = function () {
+          navigator.geolocation.clearWatch(watchID);
+        };
+
+        q.promise.clearWatch = function (id) {
+          navigator.geolocation.clearWatch(id || watchID);
+        };
+
+        q.promise.watchID = watchID;
+
+        return q.promise;
+      },
+
+      clearWatch: function (watchID) {
+        return navigator.geolocation.clearWatch(watchID);
+      }
+    };
+  }]);
